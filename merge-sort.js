630eef3c8e85213cabfb94ba5b7e9abdaff8b482
earnestabromjr <abromearnest@gmail.com>
2025-12-08 10:07:48 -0600

    Started working on mergeSort module

--- merge-sort.js
+++ merge-sort.js
@@ -1,1 +1,8 @@
-const mergeSort = (target) => {};
+function mergeSort(array) {
+	if (array.length == 1) return array;
+	const midpoint = Math.ceil(array.length / 2);
+	const firstHalf = array.slice(0, midpoint);
+	const secondHalf = array.slice(midpoint);
+}
+
+mergeSort([1, 5, 8, 0, 7]);
