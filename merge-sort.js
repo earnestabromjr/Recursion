786feab943fc4991ef87916dbb421f2cf933e18c
earnestabromjr <abromearnest@gmail.com>
2025-12-09 13:06:14 -0600

    Finished mergesort funcion

--- merge-sort.js
+++ merge-sort.js
@@ -1,8 +1,27 @@
 function mergeSort(array) {
-	if (array.length == 1) return array;
-	const midpoint = Math.ceil(array.length / 2);
-	const firstHalf = array.slice(0, midpoint);
-	const secondHalf = array.slice(midpoint);
+  if (array.length == 1) return array;
+  const midpoint = Math.ceil(array.length / 2);
+  const firstHalf = array.slice(0, midpoint);
+  const secondHalf = array.slice(midpoint);
+  for (let i = 0; i < firstHalf.length; i++) {
+    for (let j = 0; j < secondHalf.length; j++) {
+      if (firstHalf[i] < secondHalf[j]) {
+        const temp = firstHalf[i];
+        firstHalf[i] = secondHalf[j];
+        secondHalf[j] = temp;
+      }
+    }
+  }
+  return mergeSort(firstHalf).concat(mergeSort(secondHalf));
 }
 
-mergeSort([1, 5, 8, 0, 7]);
+let randomArray = [];
+let seed = 10;
+
+for (let i = 0; i < seed; i++) {
+  randomArray[i] = Math.ceil(Math.random() * 10);
+}
+// console.log(mergeSort(randomArray));
+
+const sortedArray = mergeSort([1, 5, 8, 0, 7]);
+console.log(sortedArray);
